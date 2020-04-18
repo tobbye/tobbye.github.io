c269b3ce9d8dd1f075a891cdbffbc9b2e2e049a4
tobbye <geeaff@qq.com>
2020-04-18 23:32:22 +0800

    新布局

--- zx-html/page/view/view-setting.js
+++ zx-html/page/view/view-setting.js
@@ -1,18 +1,32 @@
 
-var data, title;
-var mix = 0.9;
-var leng = 100;
-var name = 'item';
-var mode = 'initSplit';
+var data, name;
 
-var isMobile;
-var isHide = false;
-var isCenter = false;
-var zoom = 1.00;
-var zoomMobile = 2.00;
-var zoomComputer = 0.70;
+var setting = {
+    mix: 0.9,
+    leng: 100,
+    name: "item",
+    mode: "initSplit",
+    view: "isFlex",
+    orgText: ["堆叠", "平铺", "直流", "分流", "居中", "居左", "对齐", "分散"],
+    tgtText: ["平铺", "堆叠", "分流", "直流", "居左", "居中", "分散", "对齐"],
+
+    isPile: true,
+    isFlex: true,
+    isFlow: true,
+    isAlign: true,
+    isCenter: true,
+    isHide: false,
+    isSplit: true,
+    isMobile: false,
+    isElement: false,
+    zoom: 1.00,
+    zoomMobile: 2.00,
+    zoomComputer: 0.70,
+};
+
+
 window.onload = function() {
-
+    var name = name || setting.name;
     data = getJson(name);
     initSplit();
     setButton();
@@ -25,11 +39,18 @@ function initText() {
     window.onresize();
 }
 
+
+// initJoin: toReplace
+// initJoin: toElement - loopElement
+
+// initSplit: loopSplit - toReplace
+// initSplit: loopSplit - toElement - loopElement
+
 function initJoin() {
     var list = copyJson(data);
     var outer = Elem.get('outer');
     outer.innerHTML = '';
-    jsonToTable(outer, list, name);
+    jsonToView(outer, list, name, 0);
     resetOuter(outer);
     window.onresize();
 }
@@ -44,53 +65,6 @@ function initSplit() {
 }
 
 
-function resetOuter(outer) {
-    if (!isCenter) {
-        for (var i=0;i<outer.children.length;i++) {
-            var inner = outer.children[i];
-            inner.id = inner.className + i;
-
-            var title = inner.children[0];
-            var table = inner.children[1];
-            table.className = 'view';
-            title.style.textAlign = "left";
-        } 
-    } 
-
-    for (var i=0;i<outer.children.length;i++) {
-        var inner = outer.children[i];
-        resetInner(inner);
-    } 
-}
-
-
-function resetInner(inner) {
-    var next = inner.nextSibling;
-    if (!next || !next.style) 
-        return;
-    if (next.getAttribute("layer") == inner.getAttribute("layer")) {
-        var titleThis = inner.children[0];
-        if (!titleThis.innerHTML.endWith("]"))
-            return;
-        var tbodyThis = inner.children[1].children[0];
-        var tbodyNext = next.children[1].children[0];
-        // if (tbodyThis.children[0].children.length < 6)
-        if (tbodyThis.innerHTML.length < 4*titleThis.innerHTML.length)
-            return;
-        var title = Elem.creat("tr", tbodyThis);
-        var align = isCenter ? "center" : "left";
-        var tdstr = "<tr><td class='title' colspan='100' style='text-align:" + align + "';>";
-        title.innerHTML = tdstr + next.children[0].innerHTML + "</td></tr>";
-
-        while (tbodyNext.hasChildNodes())
-            tbodyThis.appendChild(tbodyNext.firstChild);
-        next.parentNode.removeChild(next);
-        resetInner(inner);
-    }
-
-}
-
-
 function loopSplit(outer, list, path, layer) {
     layer ++;
     var dict = "";
@@ -98,17 +72,17 @@ function loopSplit(outer, list, path, layer) {
     for (let y in list) {
         if (list[y] == null) continue;
         var length = JSON.stringify(list[y]).length;
-        if (typeof (list[y]) == 'object' && (length > leng * mix)) {
+        if (typeof (list[y]) == 'object' && (length > setting.leng * setting.mix)) {
             lines[y] = copyJson(list[y]);
             list[y] = [y];
         }
     }
-    console.log('--------------------------------');
-    console.log(path);
-    console.log(list);
-    console.log('obj.length: ' + list.length);
-    console.log('str.length: ' + JSON.stringify(list).length);
-    jsonToTable(outer, list, path, layer);
+    // console.log('--------------------------------');
+    // console.log(path);
+    // console.log(list);
+    // console.log('obj.length: ' + list.length);
+    // console.log('str.length: ' + JSON.stringify(list).length);
+    jsonToView(outer, list, path, layer);
     for (let y in lines) {
         if (/^\d+$/.test(y))
             dict = path + '[' + y + ']';
@@ -119,21 +93,154 @@ function loopSplit(outer, list, path, layer) {
 }
 
 
+function jsonToView(outer, data, title, layer) {
+    if (setting.isElement)
+        toElement(outer, data, title, layer);
+    else
+        toReplace(outer, data, title, layer);
+}
 
-function jsonToTable(outer, data, title, layer) {
+
+const objStr = '<table><tr><td><h1>';
+const arrStr = '<table><tr><td>';
+
+function toReplace(outer, data, title, layer) {
     // console.log(data);
     var str = JSON.stringify(data);
-    str = str[0] == '{' ? '[' + str + ']' : str;
 	str = str.replace(/\\n/g, '<br/>').replace(/\\/g, '');
-	str = str.replace(/\[{/g, '<table><tr><td><h1>').replace(/}]/g, ']').replace(/},/g, '}');
-	str = str.replace(/\[/g, '<table><tr><td>').replace(/]/g, '</td></tr></table>');
-	str = str.replace(/{/g, '<tr><td><h1>').replace(/}/g, '</td></tr>');
-	str = str.replace(/,/g, '</td><td><h1>').replace(/,/g, '</td><td>');
-	str = str.replace(/":/g, '</h1>').replace(/"/g, '');
-    str = "<div class='title'>" + title + "</div>" + str;
+
+    if (setting.isSplit) {
+        if (setting.isFlex) 
+            str = str.replace(/\[{/g, '{').replace(/}]/g, '}');
+        
+        str = str.replace(/{/g, '<table><tr><td><h1>').replace(/}/g, '</td></tr></table>');
+    } else {
+        str = str[0] == '{' ? '[' + str + ']' : str;
+        str = str.replace(/\[{/g, '<table><tr><td><h1>').replace(/}]/g, ']').replace(/},/g, '}');
+        str = str.replace(/{/g, '<tr><td><h1>').replace(/}/g, '</td></tr>');
+    }
+    // str = str.replace(/,\[/g, '</td><td>[').replace(/,{/g, '</td><td>{');
+    str = str.replace(/\[/g, '<table><tr><td>').replace(/]/g, '</td></tr></table>');
+    if (setting.isPile) {
+        //平铺 && 堆叠
+        str = str.replace(/,/g, '</td><td><h1>');
+        if (setting.isFlex) 
+            str = str.replace(/":/g, '</h1>');
+        else
+            str = str.replace(/":/g, '</h1>'); 
+        str = str.replace(/"/g, '');
+        str = "<h2 float='left'>" + title + "</h2>" + str;
+    } else {
+        //直流 && 分流
+        str = str.replace(/,/g, '</td></tr><tr><td><h1>');
+        if (setting.isFlow)
+            str = str.replace(/":/g, '</h1>');
+        else 
+            str = str.replace(/":/g, '</h1></td><td>'); 
+        str = str.replace(/"/g, '');
+        str = "<h2 float='top'>" + title + "</h2>" + str;
+    }
+
 	var inner = Elem.creat('div', outer, 'inner');
     inner.setAttribute('layer', layer);
 	inner.innerHTML = str;
+}
+
+
+
+function toElement(outer, data, title, layer) {
+    var inner = Elem.creat('div', outer, 'inner');
+    inner.setAttribute('layer', layer);
+    var child = Elem.creat("div", inner, "title");
+    child.innerHTML = title;
+    loopElement(inner, data, 0);
+}
+
+function loopElement(inner, data, idx) {
+    if (data == null) 
+        return;
+    inner.innerHTML += toHead(idx);
+    var table = Elem.creat("table", inner, "table");
+    var tr = Elem.creat("tr", table, "row");
+    for (let y in data) {
+        var td = Elem.creat("td", tr, "col");
+        if (typeof (data[y]) == "object")
+            loopElement(td, data[y], y);
+        else
+            td.innerHTML = toHead(y) + data[y];
+    }
+}
+
+function toHead(idx) {
+    return !isNaN(idx)? "":"<h1>" + idx + "</h1>";
+}
+
+
+
+function resetOuter(outer) {
+    for (var i=0;i<outer.children.length;i++) {
+        var inner = outer.children[i];
+        inner.id = inner.className + i;
+        var title = inner.children[0];
+        var table = inner.children[1];
+
+        //居中
+        if (!setting.isCenter) {
+
+            table.className = 'align-left';
+            title.style.textAlign = "left";
+        } 
+        //堆叠
+        while (setting.isFlex && inner.children.length > 2) {
+            var tableNext = inner.children[2];
+            var trNext = tableNext.children[0].children[0];
+            table.appendChild(trNext);
+            inner.removeChild(tableNext);
+        }
+    } 
+
+    for (var i=0;i<outer.children.length;i++) {
+        var inner = outer.children[i];
+        //对齐
+        if (setting.isAlign) {
+            setAlign(inner);
+        }
+    } 
+}
+
+
+function setAlign(inner) {
+    var next = inner.nextSibling;
+    if (!next || !next.hasChildNodes()) 
+        return;
+    if (next.getAttribute("layer") == inner.getAttribute("layer")) {
+        var thisTitle = inner.children[0];
+        if (!thisTitle.innerHTML.endWith("]"))
+            return;
+        var thisTbody = inner.children[1].children[0];
+        var nextTbody = next.children[1].children[0];
+        var thisLength = thisTbody.children[0].children.length;
+        var nextLength = nextTbody.children[0].children.length;
+        var count = (thisTbody.children.length + 1) / 2;
+        var between = thisTbody.innerHTML.length / nextTbody.innerHTML.length / count;
+        if (thisTbody.innerHTML.length < 4*thisTitle.innerHTML.length)
+            return;
+        if (nextLength < 10 && (between < 0.707 || between > 1.414))
+            return;
+        if (thisLength != nextLength)
+            return;
+
+        var title = Elem.creat("tr", thisTbody);
+        var align = setting.isCenter ? "center" : "left";
+        var tdstr = "<tr><td class='title' colspan='100'><h2 style='text-align:" + align + "';>";
+        title.innerHTML = tdstr + next.children[0].innerHTML + "</h2></td></tr>";
+
+        while (nextTbody.hasChildNodes())
+            thisTbody.appendChild(nextTbody.firstChild);
+        next.parentNode.removeChild(next);
+        setAlign(inner);
+    }
+
 }
 
 
@@ -159,66 +266,92 @@ function setButton() {
 
 function tapButton(btn) {
     var modeVal = btn.getAttribute('val-mode');
+    var viewVal = btn.getAttribute('val-view');
     var lengVal = btn.getAttribute('val-leng');
     var nameVal = btn.getAttribute('val-name');
     //run action
-    if (modeVal == "initCenter") {
-        isCenter = !isCenter;
-        eval(mode+'();');
-    } else
     if (modeVal && typeof(eval(modeVal)) == 'function') {
-        mode = modeVal;
-        eval(mode+'();');
+        setting.isSplit = modeVal == "initSplit";
+        setting.mode = modeVal;
     }
+    if (viewVal) {
+        togButtonText(btn, viewVal, "isFlex");
+        togButtonText(btn, viewVal, "isFlow");
+        togButtonText(btn, viewVal, "isAlign");
+        togButtonText(btn, viewVal, "isCenter"); 
+        setting.view = viewVal;
+    }
+
     //split length
     if (lengVal) {
-        leng = lengVal;
-        eval(mode+'();');
+        setting.leng = lengVal;
     }
     //data name
     if (nameVal) {
         name = nameVal;
-        if (name == 'testdata') 
+        setting.name = nameVal;
+        if (nameVal == 'testdata') 
             data = copyJson(testData);
         else
-            data = getJson(name);
-        eval(mode+'();');
+            data = getJson(nameVal);
     }
+
+    if (name == 'setting') 
+        data = copyJson(setting);
+    eval(setting.mode+'();');
     var nodes = btn.parentNode.childNodes;
     for (let x in nodes) {
         togButton(nodes[x]);            
     }
 }
 
+
 function togButton(btn) {
     if (!btn || !btn.style) return;
-    if (btn.getAttribute('val-mode') == mode || 
-        btn.getAttribute('val-leng') == leng || 
-        btn.getAttribute('val-name') == name) {
+    if (btn.getAttribute('val-mode') == setting.mode || 
+        btn.getAttribute('val-view') == setting.view || 
+        btn.getAttribute('val-leng') == setting.leng || 
+        btn.getAttribute('val-name') == setting.name) {
         Elem.color(btn, '#fff', 'dodgerblue');
     } else {
         Elem.color(btn, 'dodgerblue', '#eee');
     }     
 }
 
+function togButtonText(btn, viewVal, key) {
+    if (viewVal == key) {
+        console.log(viewVal + "=" + key);
+        if (viewVal == "isFlex") 
+            setting.isPile = true;
+        if (viewVal == "isFlow") 
+            setting.isPile = false;
+        setting[key] = !setting[key];
+        for (let x in setting.orgText) {
+            if (btn.innerHTML == setting.orgText[x]) {
+                btn.innerHTML = setting.tgtText[x];
+                return;
+            }
+        }
+    }
+}
 
 function back() {
     window.location.href = getJson('page');
 }
 
 window.onresize = function() {
-    var isMobile = (/Android|webOS|iPhone|iPod|BlackBerry|MIX/i.test(navigator.userAgent));
-    zoom = isMobile ? zoomMobile : zoomComputer;
+    setting.isMobile = (/Android|webOS|iPhone|iPod|BlackBerry|MIX/i.test(navigator.userAgent));
+    setting.zoom = setting.isMobile ? setting.zoomMobile : setting.zoomComputer;
     //20 = outer.paddingTop + outer.paddingBot;
-    var height = window.innerHeight / zoom - 20;
-    document.body.style.zoom = zoom;
+    var height = window.innerHeight / setting.zoom - 20;
+    document.body.style.zoom = setting.zoom;
     var outer = Elem.get('outer');
-    var child = Elem.get("flex1").children[2];
+    var child = Elem.get("flex2").children[1];
     //outer.scrollWidth超出body.inner,隐藏居中按钮
-    isHide = outer.scrollWidth * zoom > window.innerWidth;
+    setting.isHide = outer.scrollWidth * setting.zoom > window.innerWidth;
     outer.style.height = (height - 90) + 'px';
-    child.style.display = isHide ? "none" : "inline";
-    var agent = isMobile ? "mobile" : "computer";
+    // child.style.display = setting.isHide ? "none" : "inline";
+    var agent = setting.isMobile ? "mobile" : "computer";
     var outerBot = Elem.get('outer-bot');
     outerBot.setAttribute("agent", agent);
     var blocks = document.getElementsByClassName('block');
