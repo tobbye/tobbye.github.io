156de92bce5b929b69ddbddf6900fb02740426f4
tobbye <geeaff@qq.com>
2022-01-01 22:23:43 +0800

    mmm

--- page/stock/stock.js
+++ page/stock/stock.js
@@ -5,13 +5,13 @@ window.onload = function() {
 
 
 let Source = {
-    head: ['', '', '开盘', '收盘', '最低', '最高', '涨跌幅','九周期', 'N阳', '三阳'],
+    head: ['序号', '代码', '名称', '开盘', '收盘', '涨跌幅', '压力位', '上穿'],
     periodStr: ['前一月','前一周','前一日'],
     period: ['hrefName', 'hrefDay', 'hrefWeek', 'hrefMonth', 'href30Min', 'href60Min'],
     hrefName: 'http://hq.sinajs.cn/list=#market#code',
-    hrefDay: 'http://q.stock.sohu.com/hisHq?code=cn_#code&start=20200101&end=20880808&stat=1&order=A&period=d&callback=Stock.queryDay&rt=jsonp',
-    hrefWeek: 'http://q.stock.sohu.com/hisHq?code=cn_#code&start=20190101&end=20880808&stat=1&order=A&period=w&callback=Stock.queryWeek&rt=jsonp',
-    hrefMonth: 'http://q.stock.sohu.com/hisHq?code=cn_#code&start=20160101&end=20880808&stat=1&order=A&period=m&callback=Stock.queryMonth&rt=jsonp',
+    hrefDay: 'http://q.stock.sohu.com/hisHq?code=cn_#code&start=20200101&end=#end&stat=1&order=A&period=d&callback=Stock.queryDay&rt=jsonp',
+    hrefWeek: 'http://q.stock.sohu.com/hisHq?code=cn_#code&start=20190101&end=#end&stat=1&order=A&period=w&callback=Stock.queryWeek&rt=jsonp',
+    hrefMonth: 'http://q.stock.sohu.com/hisHq?code=cn_#code&start=20180101&end=#end&stat=1&order=A&period=m&callback=Stock.queryMonth&rt=jsonp',
     href30Min: 'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=#market#code&scale=30&datalen=1023&callback=Stock.query30Min&rt=jsonp',
     href60Min: 'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=#market#code&scale=60&datalen=1023&callback=Stock.query60Min&rt=jsonp',
 }
@@ -27,25 +27,14 @@ let VAL = {
     VOL:7,
     AMOUNT:8,
     SWAPE:9,
+    MA3:10,
+    TOP:11,
+    MID:12,
+    BOT:13,
+    LINE:14,
+    CROSS:15,
 }
 
-let TAL = {
-    DATE:0,
-    OPEN:1,
-    CLOSE:2,
-    LOW:3,
-    HIGH:4,
-    DEG:5,    
-
-    MA5:6,
-    MA10:7,
-    MA20:8,
-    MA30:9,
-    MA60:10,
-    NINESTATE:11,
-    NINECOUNT:12,
-    CURRSTATE:13,
-}
 
 
 
@@ -58,29 +47,40 @@ function __Stock() {
         this.outer = Elem.get('outer');
         this.input = Elem.get('input');
         this.tbody = Elem.get('tbody');
-        this.name = Elem.get('name');
+        this.thead = Elem.get('thead');
+        this.head = document.getElementsByTagName('head')[0];
         this.dateMonth = [];
         this.dateWeek = [];
         this.dateDay = [];
-        this.ninePeriod = [];
         this.loop = 280;
-        this.initHREF();
+        this.getCode();
         console.log(this);
+    }
+
+    this.getCode = function() {
+        let json = JSON.parse(localStorage.getItem('queryCodes'));
+        this.json = json;
+        if (json.codes.length > json.cur) {
+            this.cur = json.cur;
+            this.end = json.date;
+            this.code = json.codes[json.cur][1];
+            this.codeName = json.codes[json.cur][2]; 
+            this.initHREF();
+        }
+        this.showData();
     }
 
     this.initHREF = function() {
         this.src = [];
         this.script = [];
-        this.head = document.getElementsByTagName('head')[0];
-        this.code = localStorage.getItem('query-code');
-        this.input.value = this.code;
         this.tbody.innerHTML = '';
         this.market = this.code[0] == '6'?'sh':'sz';
-        this.nameEval = 'hq_str_' + this.market + this.code;
         for (let i in Source.period) {
             this.src[i] = Source[Source.period[i]]
             .replace('#market', this.market)
-            .replace('#code', this.code);
+            .replace('#start', this.start)
+            .replace('#code', this.code)
+            .replace('#end', this.end);
 
             if (this.script[i]) 
                 this.script[i].parentNode.removeChild(this.script);
@@ -89,37 +89,28 @@ function __Stock() {
             this.script[i].src = this.src[i];
         }
         setTimeout(function() {
-            Stock.name.innerHTML = eval(Stock.nameEval).split(',')[0];
-            Source.head[0] = Stock.code;
-            Source.head[1] = Stock.name.innerHTML;
-            Stock.tbody.innerHTML += '<tr idx=C6><td>' + Source.head.join('</td><td>') + '</td></tr>';
-        }, 500);
-        setTimeout(function() {
-            Stock.curIdx = Stock.dateDay.length;
-            Stock.calcData();
+            Stock.judge();
         }, 2000);
     }
 
 
 
-    this.reload = function(isAll) {
-    	this.isAll = isAll;
-        localStorage.setItem('query-code',this.input.value);
-        window.location.reload();
-    }
+
 
     this.queryDay = function(res) {
         this.queryData(res[0].hq, 1);
     }
 
     this.queryWeek= function(res) {
-        this.queryData(res[0].hq, 2);
+        if (res[0])
+            this.queryData(res[0].hq, 2);
     }
 
     this.queryMonth = function(res) {
         if (eval(res.status) == 2) 
             return alert('股票代码' + this.code + '不存在!');
-        this.queryData(res[0].hq, 3);
+        if (res[0])
+            this.queryData(res[0].hq, 3);
     }
 
     this.query30Min = function(res) {
@@ -135,122 +126,83 @@ function __Stock() {
 
         for (let i in origin) {
         	if (i==0) continue;
-        	let zdf = this.zdf(origin,i);
-            let temp = [
-                origin[i][VAL.DATE],
-                eval(origin[i][VAL.OPEN]),
-                eval(origin[i][VAL.CLOSE]),
-                eval(origin[i][VAL.LOW]),
-                eval(origin[i][VAL.HIGH]),
-                ~~(10000*zdf-10000)/100 + '%',
-                this.getMA(origin, i, 5),
-                this.getMA(origin, i, 10),
-                this.getMA(origin, i, 20),
-                this.getMA(origin, i, 30),
-                this.getMA(origin, i, 60),
-            ];
+            origin[i][VAL.OPEN]  = eval(origin[i][VAL.OPEN]);
+            origin[i][VAL.CLOSE] = eval(origin[i][VAL.CLOSE]);
+            origin[i][VAL.LOW]   = eval(origin[i][VAL.LOW]);
+            origin[i][VAL.HIGH]  = eval(origin[i][VAL.HIGH]);
+            origin[i][VAL.DEEPTH]   = 0;
             if (idx == 1) {
-                this.dateDay.push(temp);
+                this.dateDay = origin;
             }
             if (idx == 2) {
-                this.dateWeek.push(temp);
+                this.setLadd(origin,i);
+                this.dateWeek = origin;
             }
             if (idx == 3) {
-                this.dateMonth.push(temp);
+                this.dateMonth = origin;
             }
         }
     }
 
-    this.getMA = function(data, i, day) {
-	    if (i >= day-1) {
-	    	let sum = 0;
-	    	for (let j=0; j<day; j++) {
-	    		sum += eval(data[i-j][VAL.CLOSE]);
-	    	}
-	    	return ~~(100*sum / day)/100;
-	    }
-	    return 0;
+    this.judge = function() {
+        let curDay = this.dateDay[this.dateDay.length-1] || [];
+        let preDay = this.dateDay[this.dateDay.length-2] || [];
+        let curWeek = this.dateWeek[this.dateWeek.length-1] || [];
+        if (curWeek[VAL.LINE] && 
+            curDay[VAL.CLOSE] > curWeek[VAL.LINE] && 
+            preDay[VAL.CLOSE] < curWeek[VAL.LINE]) {
+            curWeek[VAL.CROSS] = true;
+        }
+        this.curDay = curDay;
+        this.curWeek = curWeek;
+        this.reload();
     }
 
-    this.Qtds = function(data,i) {
-    	let ha = Math.max(data[i][TAL.MA20],data[i][TAL.MA30])<data[i][TAL.MA60];
-    	let hb = data[i-1][TAL.CLOSE]<data[i-1][TAL.MA60];
-    	let hc = data[i][TAL.CLOSE]>data[i][TAL.MA60] && this.zdf(data,i)>1.12;
+
+    this.reload = function() {
+        let json = JSON.parse(localStorage.getItem('queryCodes'));
+        let curCode = json.codes[json.cur];
+        curCode.push(this.curDay[VAL.OPEN]);
+        curCode.push(this.curDay[VAL.CLOSE]);
+        curCode.push(this.curDay[VAL.DEGREE]);
+        curCode.push(this.curWeek[VAL.LINE]);
+        curCode.push(this.curWeek[VAL.CROSS]);
+        json.codes[json.cur] = curCode;
+        json.cur += 1;
+        localStorage.setItem('queryCodes',JSON.stringify(json));
+        window.location.reload();
     }
+
+
+    this.showData = function() {
+        this.thead.innerHTML = this.toDate(this.json.date) + ' · 涨停分析';
+        this.tbody.innerHTML += '<tr><td>' + Source.head.join('</td><td>') + '</td></tr>';
+        for (let i in this.json.codes) {
+            let tr = Elem.creat('tr', this.tbody, 'tr');
+            for (let j in this.json.codes[i]) {
+                let td = Elem.creat('td', tr, 'td');
+                td.innerHTML = this.json.codes[i][j];
+            }
+        }
+    }
+
+    this.toDate = function(date) {
+        return date.substring(0,4) + '年' + date[4]+date[5] + '月' + date[6]+date[7] + '日';
+    }
+
 
     this.zdf = function(data, i) {
-    	return data[i][TAL.CLOSE]/data[i-1][TAL.CLOSE];
+        return data[i][TAL.CLOSE]/data[i-1][TAL.CLOSE];
     }
 
-    this.everyLess = function(data, i, a, b, day) {
-    	for (let j=0; j<day; j++) {
-    		if (Math.max(data[i-j][TAL.MA5],data[i][TAL.MA10])>data[i][TAL.MA60])
-    			return 0;
-    	}
-    	return 1;
+    this.isYang = function(data) {
+    	return data[TAL.CLOSE]>=data[TAL.OPEN];
     }
 
-
-    this.calcData = function() {
-        this.curIdx += -1;
-        this.curDay = this.curIdx;
-        for (let i in this.dateWeek) {
-            if (this.compary(this.dateDay[this.curIdx][VAL.DATE], this.dateWeek[i][VAL.DATE])) {
-                this.curWeek = ~~i;
-                break;
-            }
-        }
-        for (let i in this.dateMonth) {
-            if (this.compary(this.dateDay[this.curIdx][VAL.DATE], this.dateMonth[i][VAL.DATE])) {
-                this.curMonth = ~~i;
-                break;
-            }
-        }
-
-        this.nineState = [];
-        this.nineCount = [];
-        this.currState = [];
-        this.conString = [];
-        if (!this.isAll && !this.isZtb(this.dateDay[this.curDay]))
-        	return this.calcNext();
-        this.getCount(this.dateMonth, this.curMonth,0);
-        if (this.nineCount[0] < 6 || this.currState[0] == 0)
-            return this.calcNext();
-        this.getCount(this.dateWeek, this.curWeek, 1);
-        if (this.nineCount[1] < 6 || this.currState[1] == 0)
-            return this.calcNext();
-        this.getCount(this.dateDay, this.curDay, 2);
-        if (this.nineCount[2] < 6 || this.currState[2] == 0)
-            return this.calcNext();
-
-        this.content = '<tr><td></td></tr><tr><td></td><td>';
-        this.content += this.dateDay[this.curDay].slice(0,6).join('</td><td>');
-        if (!this.isZtb(this.dateDay[this.curDay-1]))
-        	this.content += '</td><td>FIRST</td></tr>';
-        else
-        	this.content += '</td><td>SECOND</td></tr>';
-        this.content += this.reverse(this.conString).join('');
-        if (this.getMin(this.nineCount) <= 6)
-            this.content = this.content.replace(/<tr/g, '<tr idx=C6');
-        if (this.getMin(this.nineCount) == 7)
-            this.content = this.content.replace(/<tr/g, '<tr idx=C7');
-        if (this.getMin(this.nineCount) == 8) 
-            this.content = this.content.replace(/<tr/g, '<tr idx=C8');
-        this.tbody.innerHTML += this.content;
-        this.tbody.lastChild.scrollIntoView();
-
-        setTimeout(function() {
-            Stock.calcNext();
-        },20);  
+    this.isZtb = function(data) {
+    	return this.zdf > 1.096 && data[TAL.CLOSE]==data[TAL.HIGH];
     }
 
-    this.calcNext = function() {
-        if (this.dateDay.length - this.curIdx < 200) {
-            this.calcData();
-        } else {
-            this.tbody.innerHTML += '<tr idx=C6><td>' + Source.head.join('</td><td>') + '</td></tr>';
-        }
-    }
 
     this.getMin = function(arr) {
         let min = arr[0];
@@ -262,46 +214,58 @@ function __Stock() {
     }
 
 
-    this.getCount = function(data, idx, k) {
-        this.nineState[k] = '';
-        this.nineCount[k] = 0;
-        for (i=idx-9;i<idx;i++) {
-            this.nineState[k] += ~~this.isYang(data[i]);
-            if (this.isYang(data[i]))
-                this.nineCount[k] ++;
+    this.getAVG = function(data, i, day) {
+        if (i >= day-1) {
+            let sum = 0;
+            for (let j=0; j<day; j++) {
+                sum += eval(data[i-j][VAL.CLOSE])/3;
+                sum += eval(data[i-j][VAL.HIGH])/3;
+                sum += eval(data[i-j][VAL.LOW])/3;
+            }
+            return this.to2f(sum / day);
         }
-        this.currState[k] = this.isYang(this.dateDay[this.curDay]);
-        let temp = data[idx-1].slice(0,6);
-        temp.push(this.nineState[k]);
-        temp.push(this.nineCount[k]);
-        temp.push(this.currState[k]);
-        this.conString[k] = '<tr><td>' + Source.periodStr[k] + '</td><td>';
-        this.conString[k] += temp.join('</td><td>') + '</td></tr>';
+        return 0;
     }
 
 
+    this.tDay = 30;
+    this.bDay = 30; 
+    this.lDay = 50; 
+    this.setLadd = function(data, idx) {
+        if (idx <= this.lDay)
+            return [0,0,0,0];
+        data[idx].push(this.getAVG(data, idx, 3));
+        let top = 0;
+        for (let i=idx-this.tDay; i<=idx; i++) {
+            if (top < data[i][VAL.MA3]) {
+                top = data[i][VAL.MA3];
+            }
+        }
+        let bot = top;
+        for (let i=idx-this.bDay; i<=idx; i++) {
+            if (bot > data[i][VAL.MA3]) {
+                bot = data[i][VAL.MA3];
+            }
+        }
+        let high = 0;
+        for (let i=idx-this.lDay; i<=idx; i++) {
+            if (high < data[i][VAL.CLOSE]) {
+                high = data[i][VAL.CLOSE];
+            }
+        }
 
-    this.isYang = function(data) {
-    	return data[TAL.CLOSE]>=data[TAL.OPEN];
+        let mid = this.to2f(top/2 + bot/2);
+        data[idx].push(top);
+        data[idx].push(mid);
+        data[idx].push(bot);
+        if (high < top*1.15 && data[idx-1][VAL.TOP] == data[idx-9][VAL.TOP])
+            data[idx].push(this.to2f(top*1.1));
+        if (!data[idx][VAL.LINE] && data[idx-1][VAL.LINE] && top <= data[idx-1][VAL.TOP])
+            data[idx].push(data[idx-1][VAL.LINE]);
     }
 
-    this.isZtb = function(data) {
-    	return data[TAL.CLOSE]==data[TAL.HIGH];
-    }
+    this.getLine = function() {
 
-
-    this.getHAL = function(data, idx) {
-        let h = 0;
-        for (let i=idx-20; i<idx; i++) {
-            if (data[i][VAL.HIGH] > h)
-                h = dat[i][VAL.HIGH];
-        }
-        let l = h;
-        for (let i=idx-20; i<idx; i++) {
-            if (data[i][VAL.LOW] < l)
-                l = dat[i][VAL.LOW];
-        }
-        return [h, l];
     }
 
 
@@ -313,22 +277,8 @@ function __Stock() {
     }
 
 
-
-
-
-
-
     this.to2f = function(val) {
         return ~~(val * 100) / 100;
-    }
-
-    this.topct = function(val, cost) {
-
-        return ~~((val - cost) / cost * 10000) / 100;
-    }
-
-    this.toval = function(str) {
-        return eval(str.replace('%',''))
     }
 
 
